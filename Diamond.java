9fa33dc1af6b715eb01efb39fecb9f0ab4ea6cf6
Rizaldi rachmansyah <rizaldirachmansyah17@gmail.com>
2016-03-31 11:01:51 +0700

    Added files via upload

---
+++ Diamond.java
@@ -0,0 +1,35 @@
+
+package diamond;
+
+import java.util.Scanner;
+
+
+public class Diamond {
+    private static int j;
+
+
+    public static void main(String[] args) {
+    Scanner scn = new Scanner (System.in);
+        System.out.println("Masukan angka : ");
+        int a = scn.nextInt();
+    for (int i = 2; i < 10; i += 2) {
+      for (int j = 0; j < 9 - i / 2; j++)
+        System.out.print(" ");
+
+      for (int j = 0; j < i; j++)
+        System.out.print("*");
+
+      System.out.print("\n");
+    }
+
+    for (int i = 8; i > 0; i -= 2) {
+      for (int j = 0; j < 9 - i / 2; j++)
+        System.out.print(" ");
+
+      for (int j = 0; j < i; j++)
+        System.out.print("*");
+
+      System.out.print("\n");
+    }
+  }
+}
